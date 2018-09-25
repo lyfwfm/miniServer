5256d161b5814e40d121df5303a085ac0025aea5
lyfwfm <lyfwfm@163.com>
2018-09-25 11:47:27 +0800

    启动时设置ETS数量

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-erl  +K true +t 5000000  +Q 1048576 +P 1048576 -name mini@127.0.0.1 -setcookie mini_server -pa ebin deps/ebin app -s tk start -detached
+erl  +K true +t 5000000  +Q 1048576 +P 1048576 -env ERL_MAX_ETS_TABLES 999999 -name mini@127.0.0.1 -setcookie mini_server -pa ebin deps/ebin app -s tk start -detached
